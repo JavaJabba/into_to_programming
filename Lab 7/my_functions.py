b254e74e0b8909f442b05c3c822660a6ca4f58b5
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2021-11-19 13:05:41 +0000

    lab 7

--- Lab 7/my_functions.py
+++ Lab 7/my_functions.py
@@ -15,40 +15,46 @@ def count(my_list, value):
     :param value: - <value> to search for
     :return:
     '''
-    # set counter
-    i = 0
-    # accumulator to count how many times <value> occurs
-    # set to zero to cover no <value> in <list>
-    num_values = 0
-    # loop over the length of the <list>
-    while i < len(my_list):
-        # if <value> and <list> index i are the same
-        if my_list[i] == value:
-            # increment the accumulator
-            num_values += 1
-        # increment the counter
-        i += 1
-    # return how many times <value> occurs in <list>
-    return num_values
+    try:
+        # set counter
+        i = 0
+        # accumulator to count how many times <value> occurs
+        # set to zero to cover no <value> in <list>
+        num_values = 0
+        # loop over the length of the <list>
+        while i < len(my_list):
+            # if <value> and <list> index i are the same
+            if my_list[i] == value:
+                # increment the accumulator
+                num_values += 1
+            # increment the counter
+            i += 1
+        # return how many times <value> occurs in <list>
+        return num_values
+    except:
+        return "houston, we have a problem!"
 
-def index(my_list:str, value):
+
+def index(my_list: str, value):
     '''
     b. index(my_list, value)-function to return the first index that value occurs in my_list.  
        Return -1 if the value does not occur in my_list
         i.index("hello", "o") -> 4
         ii.index("hello", "p") -> -1
     '''
-    
-    i = 0
-    while i <= len(my_list) -1:
-        if value == my_list[i]:
-            return i
-        elif value not in my_list:
-            return "-1"
-        else:
-            i+=1
+    try:
+        i = 0
+        while i <= len(my_list) - 1:
+            if value == my_list[i]:
+                return i
+            elif value not in my_list:
+                return "-1"
+            else:
+                i += 1
+    except:
+        return "houston, we have a problem!"
 
-def get_value(my_list:str, index:int):
+def get_value(my_list: str, index: int):
     '''
     c. get_value(my_list, index) -function to return the value that occurs in my_list at index. 
        If index does not return a valid value from my_list, it should return the error message as outlined in item3 below.
@@ -62,6 +68,7 @@ def get_value(my_list:str, index:int):
     except:
         return "houston, we have a problem!"
 
+
 def insert(my_list, index, value):
     '''
     d. insert(my_list, index, value) 
@@ -70,14 +77,68 @@ def insert(my_list, index, value):
             i.insert("hello", 1, "a") -> "hallo"
             ii.insert("hello", 5, "p") -> "hellop"
     '''
-    list_size = len(my_list)
-    i = 0
-    while i <= list_size:
-        if i == index:
-            [i] = value
-            return my_list
-        elif index > len(my_list):
-            [i] = value
-            return my_list
-        i+=1
+    try:
+        i = 0
+        while i <= len(my_list):
+            if i == index:
+                first_half = my_list[:i]
+                second_half = my_list[i+1:]
+                my_list = first_half + value + second_half
+                return my_list
+            elif index > len(my_list):
+                new_list = my_list + value
+                return new_list
+            i += 1
+    except:
+        return "houston, we have a problem!"
+
+
+def value_in_list(my_list, value):
+    '''
+    e. value_in_list(my_list, value) - function to return True or False if value occurs in my_list
+        i. we can then use "if value_in_list(list, value):" as a boolean check
+        ii. value_in_list("hello", "o") - True
+        iii. value_in_list("hello", "a") - False
+    '''
+    try:
+        i = 0
+        while i <= len(my_list):
+            if my_list[i] == value:
+                return True
+            elif my_list[i] != value:
+                i += 1
+    except:
+        return False
+
+def concat(list1, list2):
+    '''
+    f. concat(list1, list2) - function to return a new list, which is a combination of list1 and list2
+        i. concat("hello", " world") -> "hello world"
+    '''
+    try:
+        if list1 and list2 == str:
+            new_list = list1 + list2
+            return new_list
+        else:
+            new_list = str(list1) + str(list2)
+            return new_list
+    except:
+        return "houston, we have a problem!"
+
+def remove(value, my_list):
+    '''
+    g. remove(value, my_list) - function to return a list with the first occurrence of value removed from my_list
+        i. remove("h", "hello") -> "ello"
+    '''
+    try:
+        i = 0
+        while i <= len(my_list):
+            if my_list[i] == value:
+                first_half = my_list[:i]
+                second_half = my_list[i+1:]
+                my_list = first_half + second_half
+                return my_list
+            i =+ 1
+    except:
+        return "houston, we have a problem!"
 
